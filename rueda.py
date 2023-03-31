233bc58e0a831da444ceeefff210fb821a8274d5
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-03-31 14:42:25 -0600

    rueda3

--- rueda.py
+++ rueda.py
@@ -2,24 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 in1 = 6   # Pin que controla el sentido de giro Motor A
-in2 = 5   # Pin que controla el sentido de giro Motor A
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)   # Configura los pines como salida
-GPIO.setup(in2, GPIO.OUT)
-
-
 
 while True:
-    #opcion=input("Escoje una opcion: ")
-    #if (opcion == 1):  # Si el comando es '1'
-        GPIO.output(in1, GPIO.LOW)  # GIRO DERECHA
-        GPIO.output(in2, GPIO.HIGH)
-    # elif (opcion == 2):  # Si el comando es '2'
-    #     GPIO.output(in1, GPIO.HIGH)  # GIRO IZQUIERDA
-    #     GPIO.output(in2, GPIO.LOW)
-    # elif (opcion == 3):  # Si el comando es '3'
-    #     GPIO.output(in1, GPIO.LOW)  # PARA
-    #     GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in1, GPIO.HIGH)  # Enciende el motor
+    time.sleep(1)  # Espera 1 segundo
+    GPIO.output(in1, GPIO.LOW)  # Apaga el motor
+    time.sleep(1)  # Espera 1 segundo
+
+GPIO.cleanup()  # Limpia los pines GPIO al salir del programa
 
 
