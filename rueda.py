efa250c5e177d53277a7b8ef98e24ce049103800
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-04-03 01:28:48 -0600

    rueda8

--- rueda.py
+++ rueda.py
@@ -1,29 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define los pines que controlan el puente H
-in1 = 5
-in2 = 6
+in1 = 4  # Pin que controla el sentido de giro Motor A
+in2 = 5  # Pin que controla el sentido de giro Motor A
 
-# Configura la Raspberry Pi
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in1, GPIO.OUT)  # Configura los pines como salida
 GPIO.setup(in2, GPIO.OUT)
 
+print("Escoje una opcion")
+
 while True:
-    comando = input("Ingresa un comando (1, 2 o 3): ")
-    
+    comando = input("Escoje una opcion: ")  # Lee el comando entrante
     if comando == '1':  # Si el comando es '1'
-        GPIO.output(in1, GPIO.LOW)  # Giro hacia la derecha
+        GPIO.output(in1, GPIO.LOW)  # GIRO DERECHA
         GPIO.output(in2, GPIO.HIGH)
     elif comando == '2':  # Si el comando es '2'
-        GPIO.output(in1, GPIO.HIGH)  # Giro hacia la izquierda
+        GPIO.output(in1, GPIO.HIGH)  # GIRO IZQUIERDA
         GPIO.output(in2, GPIO.LOW)
     elif comando == '3':  # Si el comando es '3'
-        GPIO.output(in1, GPIO.LOW)  # Para el motor
+        GPIO.output(in1, GPIO.LOW)  # PARA
         GPIO.output(in2, GPIO.LOW)
-        
-    time.sleep(0.1)  # Pequeña pausa para evitar la sobrecarga del procesador
-
-
+    else:
+        print("Comando no válido")
+    
+    time.sleep(0.1)  # Pequeña pausa para evitar la sobrecarga del puerto serial
 
