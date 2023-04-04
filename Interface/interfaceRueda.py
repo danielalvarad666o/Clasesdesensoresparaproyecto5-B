5f26adc068b06c661571b6c6c0908811490e7589
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-04-04 12:43:32 -0600

    rueda13

---
+++ Interface/interfaceRueda.py
@@ -0,0 +1,19 @@
+from Clases import rueda
+larueda=rueda.MotorDriver()
+
+class interfaceRueda: 
+    def __init__(self) -> None:
+        print("--"*10)
+        print("1)PRENDER ALA DERECHA ")
+        print("2)PRENDER ALA IZQUIERDA ")
+        print("1)Apagar ")
+        opcion=int(input("Escoje Una Opcion: "))
+        if opcion==1:
+            larueda.go_right()
+        elif opcion==2:
+            larueda.go_left()
+        elif opcion==3:
+            larueda.stop()
+        self.prenderaladerecha()
+        
+   
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
 #importaciones---------------------------------
-import board
 from Menu import menu
-from Clases import rueda,sensoredeGas
+from Interface import interfaceRueda
+
 
 
 #Instancias------------------------------------
-
 elmenu=menu.interfasMenu()
-larueda=rueda.MotorDriver()
-eldegas=sensoredeGas.MQ4(26)
+
+
+
 
 
 #variables---------------------------------------
@@ -20,8 +20,9 @@ while opcion!=2:
      opcion=elmenu.MostrarMenu()
      
      if opcion==1:
-             larueda.go_left()
-             ppm_value = eldegas.read_ppm()
-             print("Valor de PPM: {}".format(ppm_value))
+             lainterfacerueda=interfaceRueda.interfaceRueda()
+             
+             
+            
              
          
