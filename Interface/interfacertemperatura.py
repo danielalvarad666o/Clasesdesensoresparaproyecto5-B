c36f93a662b350c7bf3e85fc88482b9d3fb39071
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-04-05 11:19:18 -0600

    DHT1

--- Interface/interfacertemperatura.py
+++ Interface/interfacertemperatura.py
@@ -7,7 +7,10 @@ class interfaceDHT:
     def __init__(self) -> None:
         self.sensordht=DHT.DHTSensor(21)
         
+        
+        
     def leertemphum(self) -> any:
+       for i in range(5):
         hu,=self.sensordht.read_humidity()
         temp=self.sensordht.read_temperature()
         self.valores={"Humedada: {}, Temperatura: {}".format(hu, temp)}
