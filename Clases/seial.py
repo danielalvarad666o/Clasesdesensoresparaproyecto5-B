bba0387b28b5cf9b072c3f42d35d4c40fcd918f1
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-04-10 10:50:39 -0600

    seriales

--- Clases/seial.py
+++ Clases/seial.py
@@ -9,19 +9,19 @@ class SerialReader:
             return self.ser.readline().decode().strip()
         else:
             print("No se ha seleccionado un puerto COM.")
-            
+    
     def select_port(self):
      ports = serial.tools.list_ports.comports()
      print("Puertos COM disponibles:")
      for port in ports:
-        if "COM" in port.device:
-            print(port.device)
+        print (ports) 
+        print(port.device)
     
      selected_port = None
      while not selected_port:
         try:
             selection = input("Selecciona un puerto COM: ")
-            if selection in [port.device for port in ports if "COM" in port.device]:
+            if selection in [port.device for port in ports]:
                 selected_port = selection
                 self.ser = serial.Serial(selected_port, 9600)
                 print(f"Puerto {selected_port} seleccionado.")
