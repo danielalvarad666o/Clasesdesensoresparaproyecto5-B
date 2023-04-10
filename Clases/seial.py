3d655693c7fc4e2300d28dc4da2259e6cac39270
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-04-10 10:39:15 -0600

    pythonserial1

--- Clases/seial.py
+++ Clases/seial.py
@@ -11,20 +11,20 @@ class SerialReader:
             print("No se ha seleccionado un puerto COM.")
     
     def select_port(self):
-        ports = serial.tools.list_ports.comports()
-        print("Puertos COM disponibles:")
-        for i, port in enumerate(ports):
-            print(f"{i+1}. {port.device}")
-        
-        selected_port = None
-        while not selected_port:
-            try:
-                selection = int(input("Selecciona un puerto COM: "))
-                if selection > 0 and selection <= len(ports):
-                    selected_port = ports[selection-1].device
-                    self.ser = serial.Serial(selected_port, 9600)
-                    print(f"Puerto {selected_port} seleccionado.")
-                else:
-                    print("Selección inválida. Inténtalo de nuevo.")
-            except ValueError:
-                print("Entrada inválida. Inténtalo de nuevo.")
+     ports = serial.tools.list_ports.comports()
+     print("Puertos COM disponibles:")
+     for port in ports:
+        print(port.device)
+    
+     selected_port = None
+     while not selected_port:
+        try:
+            selection = input("Selecciona un puerto COM: ")
+            if selection in [port.device for port in ports]:
+                selected_port = selection
+                self.ser = serial.Serial(selected_port, 9600)
+                print(f"Puerto {selected_port} seleccionado.")
+            else:
+                print("Selección inválida. Inténtalo de nuevo.")
+        except ValueError:
+            print("Entrada inválida. Inténtalo de nuevo.")
